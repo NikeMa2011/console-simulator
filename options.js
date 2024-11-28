24a11470d2c46f52434debce8993ec9e857886cb
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2024-11-28 19:29:46 +0800

    Update options.js

--- options.js
+++ options.js
@@ -2,28 +2,28 @@
 var githubLink = "https;//github.com/NikeMa2011/";
 var email = "huz193py@outlook.com";
 
-let date = new Date;//初始化
+let year, month, day, weekday, minute, second, millisecond;
 
 function run(keyWords) {
-    if(keyWords[0] == "introduce") {// 介绍
-        if(keyWords[1] == undefined) {
-            text = `Error: no language string, type language to define output language, 2 options available: english, chinese`;
-            addParagraph(text);
-        } else if(keyWords[1] == "english") {
+    if(keyWords[0] == undefined) {
+        addParagraph();
+    } else if(keyWords[0] == "introduce") {// 介绍
+        if(keyWords[1] == undefined) addParagraph("Error: no language string, type language to define output language, 2 options available: english, chinese");
+        else if(keyWords[1] == "english") {
             text = `
                 Welcome to the console simulator!<br>
                 this is a project only for practice<br>
                 by Nike Ma<br>
-                source code: ` + githubLink + `<br>
-                contact: ` + email;
+                source code: ${githubLink}<br>
+                contact: ${email}`;
             addParagraph(text);
         } else if(keyWords[1] == "chinese") {
             text = `
                 欢迎来到 控制台模拟器!<br>
                 这只是一个拿来实践的项目<br>
                 由Nike Ma制作<br>
-                源代码: ` + githubLink + `<br>
-                联系方式h: ` + email;
+                源代码: ${githubLink}<br>
+                联系方式: ${email}`;
             addParagraph(text);
         } else err();
     } else if(keyWords[0] == "help") {// 帮助
@@ -38,7 +38,7 @@ function run(keyWords) {
             introduce "language"  ^introduce this website<br>
             open  "http link"  ^open a website, able compare do http defined or not<br>
             print  ^print text
-        `;//    <br>
+        `;
         addParagraph(text);
     } else if(keyWords[0] == "open") {//打开网站
         var link = keyWords[1];
@@ -48,94 +48,59 @@ function run(keyWords) {
                 window.open(keyWords[1]);
             }, 1000);
         } else {
-            addParagraph("Warning: didnt define the http<br>opening " + keyWords[1]);
+            addParagraph(`Warning: didnt define the http<br>opening ${keyWords[1]}`);
             setTimeout(() => {
                 window.open("http://" + keyWords[1]);
             }, 1000);
         }
     } else if(keyWords[0] == "date") {// 日期
-        if(keyWords[1] == undefined) {
-            addParagraph("Error: no option string, 5 options available: year, month, day, full and all");
-        } else if(keyWords[1] == "year") {
-            addParagraph(date.getFullYear());
-        } else if(keyWords[1] == "month") {
-            addParagraph(date.getMonth() + 1);
-        } else if(keyWords[1] == "day") {
-            addParagraph(date.getDate());
-        } else if(keyWords[1] == "weekday") {
-            addParagraph(date.getDay());
-        } else if(keyWords[1] == "full") {
-            text = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+        if(keyWords[1] == undefined) addParagraph("Error: no option string, 5 options available: year, month, day, full and all");
+        else if(keyWords[1] == "year") addParagraph(year);
+        else if(keyWords[1] == "month") addParagraph(month + 1);
+        else if(keyWords[1] == "day") addParagraph(day);
+        else if(keyWords[1] == "weekday") addParagraph(weekday);
+        else if(keyWords[1] == "full") {
+            text = year + '-' + (month + 1) + '-' + day;
             addParagraph(text);
         } else if(keyWords[1] == "all") {
-            text = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getDay();
+            text = year + '-' + (month + 1) + '-' + day + ' ' + weekday;
             addParagraph(text);
         } else err();
     } else if(keyWords[0] == "time") {// 时间
-        if(keyWords[1] == undefined) {
-            addParagraph("Error: no option string, 6 options available: hour, minute, second, millisecond, full and all");
-        } else if(keyWords[1] == "hour") {
-            addParagraph(date.getHours());
-        } else if(keyWords[1] == "minute") {
-            addParagraph(date.getMinutes());
-        } else if(keyWords[1] == "second") {
-            addParagraph(date.getSeconds());
-        } else if(keyWords[1] == "millisecond") {
-            addParagraph(date.getMilliseconds());
-        } else if(keyWords[1] == "full") {
-            text = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+        if(keyWords[1] == undefined) addParagraph("Error: no option string, 6 options available: hour, minute, second, millisecond, full and all");
+        else if(keyWords[1] == "hour") addParagraph(hour);
+        else if(keyWords[1] == "minute") addParagraph(minute);
+        else if(keyWords[1] == "second") addParagraph(second);
+        else if(keyWords[1] == "millisecond") addParagraph(millisecond);
+        else if(keyWords[1] == "full") {
+            text = hour + ':' + minute + ':' + second;
             addParagraph(text);
         } else if(keyWords[1] == "all") {
-            text = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ':' + date.getMilliseconds();
+            text = hour + ':' + minute + ':' + second + ':' + millisecond;
             addParagraph(text);
         } else err();
     } else if(keyWords[0] == "when") {
-        text = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ';//日期
-        text += (date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());//时间
+        text = year + '-' + (month + 1) + '-' + day + " " + hour + ':' + minute + ':' + second + ':' + millisecond;
         addParagraph(text);
     } else if(keyWords[0] == "time-count") {// 计时
-        if(keyWords[1] == undefined) {
-            addParagraph("Error: no option string, 3 options available: hour, minute and second");
-        } else if(keyWords[1] == "hour") {
+        if(keyWords[1] == undefined) addParagraph("Error: no option string, 3 options available: hour, minute and second");
+        else if(keyWords[1] == "hour") {
             if(keyWords[2] <= 4 && keyWords[2] > 0) {
                 coutTime();
                 setTimeout(() => {
                     addParagraph("time count out");
                 }, keyWords[2] * 60 * 60 * 1000);
-            } else if(keyWords[2] > 4) {
-                addParagraph("Error: maxium input is 4");
-            } else if(keyWords[2] <= 0) {
-                addParagraph("Error: input cannot be or smaller than 0");
-            } else err();
-        } else if(keyWords[1] == "minute") {
-            if(keyWords[2] <= 60 && keyWords[2] > 0) {
-                coutTime();
-                setTimeout(() => {
-                    addParagraph("time count out");
-                }, keyWords[2] * 60 * 1000);
-            } else if(keyWords[2] > 60) {
-                addParagraph("Error: maxium input is 60");
-            } else if(keyWords[2] <= 0) {
-                addParagraph("Error: input cannot be or smaller than 0");
-            } else err();
-        } else if(keyWords[1] == "second") {
-            if(keyWords[2] <= 60 && keyWords[2] > 0) {
-                coutTime();
-                setTimeout(() => {
-                    addParagraph("time count out");
-                }, keyWords[2] * 60 * 1000);
-            } else if(keyWords[2] > 60) {
-                addParagraph("Error: maxium input is 60");
-            } else if(keyWords[2] <= 0) {
-                addParagraph("Error: input cannot be or smaller than 0");
-            } else err();
-        } else err();
-    } else if(keyWords[0] == "print") {// 输出
-        if(keyWords[1] == undefined) {
-            addParagraph("Error: no string to output");
-        } else {
-            addParagraph(keyWords[1]);
+            }
+            else if(keyWords[2] > 4) addParagraph("Error: maxium input is 4");
+            else if(keyWords[2] <= 0) addParagraph("Error: input cannot be or smaller than 0");
+            else err();
         }
+        else if(keyWords[1] == "minute") timeCount(60);
+        else if(keyWords[1] == "second") timeCount(60);
+        else err();
+    } else if(keyWords[0] == "print") {// 输出
+        if(keyWords[1] == undefined) addParagraph("Error: no string to output");
+        else addParagraph(keyWords[1]);
     } else if(keyWords[0] == "hello") {// 你好世界
         addParagraph("world!");
     } else if(keyWords[0] == "clear") {// 清空
@@ -149,3 +114,25 @@ function err() {
 function coutTime() {
     cout("time: " + keyWords[2] + keyWords[1] + "counting");
 }
+function timeCount(inputNum) {
+    if(keyWords[2] <= inputNum && keyWords[2] > 0) {
+        coutTime();
+        setTimeout(() => {
+            addParagraph("time count out");
+        }, keyWords[2] * 60 * 1000);
+    }
+    else if(keyWords[2] > inputNum) addParagraph("Error: maxium input is " + inputNum);
+    else if(keyWords[2] <= 0) addParagraph("Error: input cannot be or smaller than 0");
+    else err();
+}
+setInterval(() => {
+    let date = new Date;
+    year = date.getFullYear();
+    month = (date.getMonth() + 1);
+    day = date.getDate();
+    weekday = date.getHours();
+    hour = date.getHours();
+    minute = date.getMinutes()
+    second = date.getSeconds();
+    millisecond = date.getMilliseconds();
+}, 1000);
